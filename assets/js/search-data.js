0f74b62d672d53427bb43ee2bbad8a5eb0a8e7f6
ngadiyar93 <43225038+ngadiyar93@users.noreply.github.com>
2026-06-07 17:41:54 +0000

    Deploying to gh-pages from @ AMPERe-Lab/ampere-lab.github.io@a6bf49c71399c0d2186ead9ea1513893f58a6f6b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,75 +37,11 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/join/";
           },
-        },{id: "news-nishanth-has-successfully-defended-his-ph-d-the-degree-will-be-conferred-at-uw-madison-s-may-2023-commencement-nishanth-will-be-continuing-as-a-post-doctoral-research-associate-with-prof-severson-at-wempec",
-          title: 'Nishanth has successfully defended his Ph.D. The degree will be conferred at UW-Madison’s...',
-          description: "",
-          section: "News",},{id: "news-nishanth-has-joined-the-aerospace-electric-machines-laboratory-at-ge-research-in-niskayuna-ny",
-          title: 'Nishanth has joined the Aerospace Electric Machines Laboratory at GE Research in Niskayuna,...',
-          description: "",
-          section: "News",},{id: "news-nishanth-is-serving-as-the-publicity-chair-and-a-track-chair-at-ieee-iemdc-2025",
-          title: 'Nishanth is serving as the publicity chair and a track chair at IEEE...',
-          description: "",
-          section: "News",},{id: "news-two-papers-published-in-the-ieee-transactions-on-industry-applications-march-april-2024-issue-n-gadiyar-j-van-de-ven-and-e-l-severson-evaluation-of-torque-dense-electric-machine-technology-for-off-highway-vehicle-electrification-in-ieee-transactions-on-industry-applications-vol-60-no-2-pp-3062-3074-march-april-2024-n-gadiyar-a-khamitov-and-e-l-severson-design-of-electric-machine-windings-to-independently-control-multiple-airgap-harmonics-in-ieee-transactions-on-industry-applications-vol-60-no-2-pp-3039-3050-march-april-2024",
-          title: 'Two papers published in the IEEE Transactions on Industry Applications, March-April 2024 issue:...',
-          description: "",
-          section: "News",},{id: "news-nishanth-is-serving-as-the-short-courses-and-tutorials-chair-and-track-chair-at-ieee-itec-eats-2025",
-          title: 'Nishanth is serving as the short courses and tutorials chair and track chair...',
-          description: "",
-          section: "News",},{id: "news-nishanth-joins-the-oak-ridge-national-laboratory-as-a-distinguished-staff-fellow",
-          title: 'Nishanth joins the Oak Ridge National Laboratory as a Distinguished Staff Fellow.',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement4/";
-            },},{id: "news-nishanth-has-been-selected-for-the-fy25-cohort-of-the-prestigious-early-career-development-program-at-ornl",
-          title: 'Nishanth has been selected for the FY25 cohort of the prestigious early-career development...',
-          description: "",
-          section: "News",},{id: "news-new-paper-on-multi-material-am-enabled-high-speed-synr-machine-in-the-ieee-transactions-on-industry-applications",
-          title: 'New paper on Multi-material AM-enabled high-speed SynR machine in the IEEE Transactions on...',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-new-ldrd-funding-us-700k-to-develop-sustainable-multi-harmonic-electric-machines",
-          title: 'New LDRD funding (~US$ 700k) to develop sustainable multi-harmonic electric machines.',
+        },{id: "news-gadiyar-elected-general-co-chair-for-iemdc-2029",
+          title: 'Gadiyar elected General co-Chair for IEMDC 2029',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_1/";
-            },},{id: "news-nishanth-has-been-appointed-as-an-associate-editor-for-the-ieee-transactions-on-transportation-electrification",
-          title: 'Nishanth has been appointed as an Associate Editor for the IEEE Transactions on...',
-          description: "",
-          section: "News",},{id: "news-re-branding-fnu-nishanth-is-nishanth-gadiyar",
-          title: 'Re-branding...FNU Nishanth is Nishanth Gadiyar',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/Announcement_12/";
-            },},{id: "news-organizing-a-special-session-at-iemdc-2025",
-          title: 'Organizing a special session at IEMDC 2025',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_13/";
-            },},{id: "news-nishanth-has-been-appointed-as-an-associate-editor-for-the-ieee-transactions-on-industry-applications",
-          title: 'Nishanth has been appointed as an Associate Editor for the IEEE Transactions on...',
-          description: "",
-          section: "News",},{id: "news-nishanth-received-the-best-reviewer-award-from-ieee-ias-electric-machines-committee",
-          title: 'Nishanth received the Best Reviewer Award from IEEE IAS-Electric Machines Committee',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_16/";
-            },},{id: "news-paper-accepted-at-ieee-ecce-2025",
-          title: 'Paper accepted at IEEE ECCE 2025',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_17/";
-            },},{id: "news-multiple-papers-accepted-at-ieee-itec-2026",
-          title: 'Multiple papers accepted at IEEE ITEC 2026',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_20/";
-            },},{id: "news-nishanth-elected-general-co-chair-for-iemdc-2029",
-          title: 'Nishanth elected General co-Chair for IEMDC 2029',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_21/";
             },},{id: "projects-integrated-electro-hydraulic-machine-for-off-highway-vehicle-electrification",
           title: 'Integrated Electro-hydraulic Machine for Off-highway Vehicle Electrification',
           description: "A key enabler to electrify off-highway vehicles and improve their energy efficiency.",
